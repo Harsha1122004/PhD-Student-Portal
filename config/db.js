7b82a4992b87a911b30a4aa90ec5d7957919bc78
Harshith Bezawada <135497343+Harsha1122004@users.noreply.github.com>
2026-01-21 09:59:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect("mongodb+srv://phdportal:Harsha37017@cluster0.glcel9s.mongodb.net/phd?appName=Cluster0", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error("❌ MongoDB Connection Error:", error.message);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    mongoose.set("strictQuery", false);
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
-
-
